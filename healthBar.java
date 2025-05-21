3f7637b25781cae3d60a567b88f66ad8a18f3360
Blank <guoshaoxuan1@gmail.com>
2025-05-21 09:11:26 -0400

    HealthBar set

---
+++ healthBar.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class healthBar here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class healthBar extends Actor
+{
+    /**
+     * Act - do whatever the healthBar wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        GreenfootImage image = new GreenfootImage("HealthBar.png");
+        image.scale(40, 50);
+        setImage(image);
+    }
+}
